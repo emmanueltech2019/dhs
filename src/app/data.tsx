72ee3be9e7b41ad18dbe326a3b533859dc35ab36
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-22 16:00:13 +0100

    first commit

---
+++ src/app/data.tsx
@@ -0,0 +1,19 @@
+interface Data {
+    city: string;
+    state: string;
+    brand: string;
+    domain: string;
+    logo: string;
+    src: string;
+}
+const data:Data ={
+    brand:"DHS",
+    city:"Dallas",
+    state:"TX",
+    domain: "dallasherniaspecialists.com",
+    logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719068158/1Asset_54_l6gnlz.png',
+    src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d429174.12173179106!2d-97.06121201685097!3d32.82080973237199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c19f77b45974b%3A0xb9ec9ba4f647678f!2sDallas%2C%20TX%2C%20USA!5e0!3m2!1sen!2sng!4v1719068078823!5m2!1sen!2sng"
+
+}
+
+export default data
